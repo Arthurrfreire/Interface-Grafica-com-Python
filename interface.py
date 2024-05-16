fda6100c02d69f9a8e6eb412b25d1069d9b35af4
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2024-05-16 19:25:23 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -7,8 +7,10 @@ layout = [
             [sg.Text(Titulos[0]), sg.Input(size=5, key=Titulos[0])],
             [sg.Text(Titulos[0]), sg.Input(size=20, key=Titulos[1])],
             [sg.Text(Titulos[0]), sg.Combo(['Fornecedor 1', 'Fornecedor 2', 'Fornecedor 3'], key=Titulos[2])],
-            
-
-
-
+            [sg.Button('Adicionar'), sg.Button('Editar'), sg.Button('Salvar', disabled=True), sg.Button('Excluir'), sg.Exit('Sair')],
+            [sg.Table(dados, Titulos, key='tabela')]
 ]
+
+window = sg.Window('Sistema de gerencia de suplementos', layout)
+
+while True: 
