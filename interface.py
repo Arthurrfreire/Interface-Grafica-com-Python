8a3bcf8ca3833a95a0a6191c6fe77e52789c7d53
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2024-05-16 20:05:14 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -39,4 +39,43 @@ while True:
         conexao.execute("INSERT INTO SUPLEMENTO (LOTE, PRODUTO, FORNECEDOR) VALUES (?,?,?)", ([values[Titulos[0]], values]))
         conexao.commit()
         conexao.close()
-        
+
+    if event == 'Editar':
+        if values['tabela']==[]:
+            sg.popup('Nenhuma linha selecionada')
+        else:
+            editarLinha=values['tabela'] [0]
+            sg.popup('Editar linha selecionada')
+            for i in range(3):
+                window[Titulos[i]].update(values=dados[editarLinha][i])
+            window['Salvar'].update(disable=False)
+
+    if event == 'Salvar':
+        dados[editarLinha]=[values[Titulos[0]], values[Titulos[1]], values[Titulos[2]]]
+        window['tabela'].update(values=dados)
+        for i in range(3):
+            window[Titulos[i]].update(value='')
+        window['Salvar'].update(disabled=True)
+
+        conexao = sqlite3.connect(db_path)
+        conexao.execute("UPDATE SUPLEMENTO set PRODUTO = ?, FORNECEDOR = ? where LOTE = ?", ([values[Titulos[1], values[Titulos[2]]]]))
+        conexao.commit()
+        conexao.close()
+
+    if event == 'Excluir':
+        if values['tabela']==[]:
+            sg.popup('Nenhuma linha selecionada')
+        else:
+            if sg.popup_ok_cancel('Essa operacao nao pode ser desfeita. Confirma?') == 'OK':
+
+                conexao = sqlite3.connect(db_path)
+                conexao.execute("DELETE FROM SUPLEMENTO WHERE LOTE = ?;", (values[Titulos[0]],))
+                conexao.close()
+
+                del dados[values['tabela'] [0]] # Remove a linha selecionada
+                window['tabela'].update(values=dados)
+
+    if event in (sg.WIN_CLOSED, 'Sair'):
+        break
+
+window.close() 
